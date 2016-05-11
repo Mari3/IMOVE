319307111c24b12c586e7d9e34c537d5b8281a6e
Wouter Posdijk <wouter@wlan-145-94-204-175.wlan.tudelft.nl>
2016-05-11 14:37:46 +0200

    -m

--- buildAndRun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-clear
-./build.sh
-./run.sh
--- calibration/src/findCameraProjectorTransformation.hpp
+++
@@ -1,8 +0,0 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-
-#include <vector>
-
-std::vector<cv::Point2f> createChessboardCorners(const cv::Size size_checkerboard, const cv::Size resolution);
-cv::Point2f* getCornersFromCheckerboard(const std::vector<cv::Point2f> checkerboard_points, const cv::Size size_checkerboard);
-cv::Mat findCameraProjectorTransformationFromCheckerboard(const cv::Mat camera_frame, const cv::Size resolution_projector, const cv::Size size_checkerboard);
