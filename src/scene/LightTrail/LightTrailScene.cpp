20bbf00cd60f3e23d957e68d461719eba4b00d27
Wouter Posdijk <wouter@wlan-145-94-206-204.wlan.tudelft.nl>
2016-05-12 12:43:53 +0200

    Change test to contain circular orbit and show gravity point

--- src/scene/LightTrail/LightTrailScene.cpp
+++ src/scene/LightTrail/LightTrailScene.cpp
@@ -16,14 +16,22 @@ void LightTrailScene::draw(sf::RenderTarget &target) {
         circle.setPosition(trail->getLocation().x,trail->getLocation().y);
         target.draw(circle);
     }
+
+    for(auto &point : gravityPoints){
+        sf::CircleShape gCircle(2);
+        gCircle.setFillColor(sf::Color::Red);
+        gCircle.setPosition(point->location.x,point->location.y);
+        target.draw(gCircle);
+    }
 }
 
 LightTrailScene::LightTrailScene() : Scene() {
     //Initialize lists
     lightSources = vector<LightSource*>();
     lightTrails = vector<LightTrail*>();
-    lightTrails.push_back(new LightTrail(Vector2(0,0),Vector2(20,20),0));
+    lightTrails.push_back(new LightTrail(Vector2(200,200),Vector2(100,0),0));
     gravityPoints = vector<GravityPoint*>();
+    gravityPoints.push_back(new GravityPoint(Vector2(200,100),Range(.9,.1,true),11000));
     colorHoles = vector<ColorHole*>();
 
     //Add actions and conditions here
