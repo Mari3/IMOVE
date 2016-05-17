b0a21c491ee0b3947201f2d164de08c4a4c6f2e7
Gerbert van Nieuwaal <gvannieuwaal@m-industries.com>
2016-05-17 14:07:22 +0200

    Feature feedbackloop elimination

--- calibration/src/main.cpp
+++ calibration/src/main.cpp
@@ -4,6 +4,7 @@
  
 #include <iostream>
 #include <string>
+#include <queue>
 
 const unsigned char U8_WHITE = 255;
 const unsigned char U8_BLACK = 0;
@@ -59,6 +60,9 @@ int main(int argc, char* argv[]) {
 		std::cout << "Usage: <path to configuration file> <int video device> <projector resolution width> <projector resolution height>" << std::endl;
 		return EXIT_SUCCESS;
 	}
+
+	unsigned int frames_projector_camera_delay = 6;
+	double RATIO_PROJECTOR_BACKGROUND_LIGHT = 0.3;
 	
 	cv::FileStorage fs;
 	fs.open(argv[1], cv::FileStorage::READ);
@@ -72,143 +76,58 @@ int main(int argc, char* argv[]) {
 	cv::VideoCapture projector_videoreader("./test/Test 1/projector.mp4");
 	cv::namedWindow("Projector", cv::WINDOW_NORMAL);
 	cv::moveWindow("Projector", 0, 0);
+	cv::Mat frame_delay_projector;
+	cv::namedWindow("Projector delay", cv::WINDOW_NORMAL);
+	cv::moveWindow("Projector delay", 300, 0);
 	cv::Mat frame_camera;
 	cv::VideoCapture camera_videoreader("./test/Test 1/camera.mp4");
 	cv::namedWindow("Camera", cv::WINDOW_NORMAL);
-	cv::moveWindow("Camera", 400, 0);
-	cv::Mat frame_result;
-	cv::namedWindow("Result", cv::WINDOW_NORMAL);
-	cv::moveWindow("Result", 800, 0);
+	cv::moveWindow("Camera", 600, 0);
+	cv::Mat frame_projectionelimination;
+	cv::namedWindow("Projection elimination", cv::WINDOW_NORMAL);
+	cv::moveWindow("Projection elimination", 900, 0);
 	cv::Mat frame_projection;
 	cv::namedWindow("Projection", cv::WINDOW_NORMAL);
 	cv::moveWindow("Projection", 1200, 0);
-
+	
+	std::queue<cv::Mat> frames_delay_projector;
 	while (cv::waitKey(1) == NOKEY_ANYKEY && projector_videoreader.read(frame_projector) && camera_videoreader.read(frame_camera)) {
-		
-		cv::warpPerspective(
-			frame_projector,
-			frame_result,
-			camera_projector_transformation,
-			frame_camera.size(),
-			cv::INTER_LINEAR | cv::WARP_INVERSE_MAP,
-			cv::BORDER_CONSTANT,
-			cv::Scalar(U8_BLACK, U8_BLACK, U8_BLACK)
-		);
-		
-		frame_result = frame_camera - frame_result;
-std::cout << frame_projector.size() << frame_camera.size() << frame_result.size() << frame_projection.size() << std::endl;
-		cv::warpPerspective(
-			frame_result,
-			//frame_camera,
-			frame_projection,
-			camera_projector_transformation,
-			resolution_projector,
-			cv::INTER_LINEAR,
-			cv::BORDER_CONSTANT,
-			cv::Scalar(U8_BLACK, U8_BLACK, U8_BLACK)
-		);
 		cv::imshow("Projector", frame_projector);
 		cv::imshow("Camera", frame_camera);
-		cv::imshow("Result", frame_result);
-		cv::imshow("Projection", frame_projection);
+		
+		frames_delay_projector.push(frame_projector.clone());
+		if (frames_delay_projector.size() > frames_projector_camera_delay) {
+			frame_delay_projector = frames_delay_projector.front();
+			frames_delay_projector.pop();
+			cv::imshow("Projector delay", frame_delay_projector);
+			cv::warpPerspective(
+				frame_delay_projector,
+				frame_projectionelimination,
+				camera_projector_transformation,
+				frame_camera.size(),
+				cv::INTER_LINEAR | cv::WARP_INVERSE_MAP,
+				cv::BORDER_CONSTANT,
+				cv::Scalar(U8_BLACK, U8_BLACK, U8_BLACK)
+			);
+			
+			frame_projectionelimination = frame_camera - (frame_projectionelimination * RATIO_PROJECTOR_BACKGROUND_LIGHT);
+			
+			cv::warpPerspective(
+				frame_projectionelimination,
+				frame_projection,
+				camera_projector_transformation,
+				resolution_projector,
+				cv::INTER_LINEAR,
+				cv::BORDER_CONSTANT,
+				cv::Scalar(U8_BLACK, U8_BLACK, U8_BLACK)
+			);
+			cv::imshow("Projection elimination", frame_projectionelimination);
+			cv::imshow("Projection", frame_projection);
+		}
 	}
 
 	projector_videoreader.release();
 	camera_videoreader.release();
-
-	/*	
-	cv::Mat frame_projector;
-	cv::namedWindow("Projector", cv::WINDOW_NORMAL);
-	cv::moveWindow("Projector", 0, 0);
-
-	cv::Mat frame_camera;
-	cv::VideoCapture camera_video_reader(std::stoi(argv[2]));
-	cv::VideoCapture projector_video_reader("./calibration/test/camera_projector_transformation/big_buck_bunny_1080p_h264.mov");
-	cv::namedWindow("Camera", cv::WINDOW_NORMAL);
-	cv::moveWindow("Camera", 500, 0);
-	cv::setMouseCallback("Camera", cameraWindowMouseEvent, NULL);
 	
-	while (amount_corners < REQUIRED_CORNERS) {
-		if (!camera_video_reader.read(frame_camera)) {
-			std::cerr << "Unable to read next frame." << std::endl;
-			std::cerr << "Exiting..." << std::endl;
-			return EXIT_FAILURE;
-		}
-		if (!projector_video_reader.read(frame_projector)) {
-			std::cerr << "Unable to read next frame." << std::endl;
-			std::cerr << "Exiting..." << std::endl;
-			return EXIT_FAILURE;
-		}
-		drawCornersOnImage(frame_camera);
-		cv::imshow("Camera", frame_camera);
-		cv::imshow("Projector", frame_projector);
-		cv::waitKey(1);
-	}
-	cv::Point2f* coordinate_corners_projector = new cv::Point2f[REQUIRED_CORNERS];
-	coordinate_corners_projector[0] = cv::Point2f(                    ORIGIN2D.x,                      ORIGIN2D.y);
-	coordinate_corners_projector[1] = cv::Point2f(resolution_projector.width - 1,                      ORIGIN2D.y);
-	coordinate_corners_projector[2] = cv::Point2f(										ORIGIN2D.x, resolution_projector.height - 1);
-	coordinate_corners_projector[3] = cv::Point2f(resolution_projector.width - 1, resolution_projector.height - 1);
-	const cv::Mat camera_projector_transformation = cv::getPerspectiveTransform(
-		coordinate_corners_camera,
-		coordinate_corners_projector
-	);
-	
-	cv::VideoWriter camera_video_writer(
-		"./test/camera.mp4",
-		cv::VideoWriter::fourcc('M', 'P', 'E', 'G'),
-		25,
-		cv::Size(camera_video_reader.get(cv::CAP_PROP_FRAME_WIDTH), camera_video_reader.get(cv::CAP_PROP_FRAME_HEIGHT)),
-		true
-	);
-	cv::VideoWriter projection_video_writer(
-		"./test/projection.mp4",
-		cv::VideoWriter::fourcc('M', 'P', 'E', 'G'),
-		25,
-		resolution_projector,
-		true
-	);
-	cv::VideoWriter projector_video_writer(
-		"./test/projector.mp4",
-		cv::VideoWriter::fourcc('M', 'P', 'E', 'G'),
-		25,
-		cv::Size(projector_video_reader.get(cv::CAP_PROP_FRAME_WIDTH), projector_video_reader.get(cv::CAP_PROP_FRAME_HEIGHT)),
-		true
-	);
-
-	cv::namedWindow("Projection", cv::WINDOW_NORMAL);
-	cv::moveWindow("Projection", 1000, 0);
-	cv::Mat frame_projection;
-	while (cv::waitKey(1) == NOKEY_ANYKEY) {
-		if (!camera_video_reader.read(frame_camera)) {
-			std::cerr << "Unable to read next frame." << std::endl;
-			std::cerr << "Exiting..." << std::endl;
-			return EXIT_FAILURE;
-		}
-		if (!projector_video_reader.read(frame_projector)) {
-			std::cerr << "Unable to read next frame." << std::endl;
-			std::cerr << "Exiting..." << std::endl;
-			return EXIT_FAILURE;
-		}
-		drawCornersOnImage(frame_camera);
-		cv::imshow("Camera", frame_camera);
-		cv::imshow("Projector", frame_projector);
-		cv::warpPerspective(
-			frame_camera,
-			frame_projection,
-			camera_projector_transformation,
-			resolution_projector
-		);
-		cv::imshow("Projection", frame_projection);
-		projector_video_writer << frame_projector;
-		projection_video_writer << frame_projection;
-		camera_video_writer << frame_camera;
-	}
-	camera_video_reader.release();
-	projector_video_reader.release();
-	camera_video_writer.release();
-	projection_video_writer.release();
-
-	*/
 	return EXIT_SUCCESS;
 }
