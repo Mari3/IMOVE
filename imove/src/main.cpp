9505e435399f80a4f9036d76658b33a3640f1186
Gerbert van Nieuwaal <gvannieuwaal@m-industries.com>
2016-05-26 11:29:24 +0200

    No threads anymore due to unsupportiveness for SFML (OS X) and OpenCV untill we seperate it into seperate processes

--- imove/src/main.cpp
+++ imove/src/main.cpp
@@ -5,8 +5,6 @@
 #include <vector>
 #include <string>
 #include <SFML/Graphics.hpp>
-#include <thread>
-#include <mutex>
 
 #include "calibration/Calibration.hpp"
 #include "image_processing/PeopleExtractor.h"
@@ -17,9 +15,6 @@
 
 const signed int NOKEY_ANYKEY = -1;
 
-std::mutex mutex_scene;
-bool running = true;
-
 Calibration* calibration;
 int camera_device;
 Scene* scene;
@@ -27,8 +22,68 @@ cv::Size resolution_camera;
 cv::Size resolution_projector;
 float meter;
 
-// Sets up people extracting and loop extracting people for scene input while running
-void main_peopleextractor() {
+// setup and run scene with continous people extraction as input based on configuration given in parameter otherwise show parameters
+int main(int argc, char* argv[]) {
+	// show parameters if not given 1 parameter
+	if (argc != 3) {
+		std::cerr << "Usage: <path to configuration file> <path to configuration scene>" << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	// read calibration config
+	cv::FileStorage fs;
+	fs.open(argv[1], cv::FileStorage::READ);
+	fs["Camera_device"] >> camera_device;
+	//cv::Size resolution_camera;
+	fs["Resolution_camera"] >> resolution_camera;
+	cv::Size resolution_projector;
+	fs["Resolution_projector"] >> resolution_projector;
+	cv::Mat camera_projector_transformation;
+	fs["Camera_projector_transformation"] >> camera_projector_transformation;
+	unsigned int frames_projector_camera_delay;
+	signed int int_frames_projector_camera_delay;
+	fs["Frames_projector_camera_delay"] >> int_frames_projector_camera_delay;
+	if (int_frames_projector_camera_delay < 0) {
+		frames_projector_camera_delay = 0;
+	} else {
+		frames_projector_camera_delay = (unsigned int) int_frames_projector_camera_delay;
+	}
+	double percentage_projector_background_light;
+	fs["Percentage_projector_background_light"] >> percentage_projector_background_light;
+	//float meter;
+	fs["Meter"] >> meter;
+	fs.release();
+
+	calibration = new Calibration(resolution_projector, camera_projector_transformation, frames_projector_camera_delay, percentage_projector_background_light);
+
+	// debug windows
+	cv::namedWindow("Camera", cv::WINDOW_NORMAL);
+	cv::moveWindow("Camera", 500, 0);
+
+	cv::namedWindow("Projection", cv::WINDOW_NORMAL);
+	cv::moveWindow("Projection", 1000, 0);
+
+	cv::namedWindow("Frame", cv::WINDOW_NORMAL);
+
+	// setup scene
+    sf::RenderWindow window(sf::VideoMode(resolution_projector.width, resolution_projector.height),"Projection");
+    window.clear(sf::Color::Black);
+    window.display();
+    
+    
+    LightTrailConfiguration config = LightTrailConfiguration::readFromFile(argv[2]);
+    scene = new LightTrailScene(config,
+                                       new LightSourceVectorRepository(),
+                                       new LightTrailVectorRepository(),
+                                       new GravityPointVectorRepository(),
+                                       new ColorHoleVectorRepository(),
+                                       new LightPersonMapRepository()
+                                       );
+
+	// setup clock
+  sf::Clock clock;
+	
+	// setup people extractor
 	PeopleExtractor people_extractor = PeopleExtractor(resolution_camera, meter, 216);
 
 	// debug camera window
@@ -101,86 +156,9 @@ void main_peopleextractor() {
 		cv::imshow("Projection", frame_projection);
 
 		// update scene with location of people
-		mutex_scene.lock();
 		scene->updatePeople(detected_people);
-		mutex_scene.unlock();
-	}
-	running = false;
-
-	pthread_exit(NULL);
-}
-
-// setup and run scene with continous people extraction as input based on configuration given in parameter otherwise show parameters
-int main(int argc, char* argv[]) {
-	// show parameters if not given 1 parameter
-	if (argc != 3) {
-		std::cerr << "Usage: <path to configuration file> <path to configuration scene>" << std::endl;
-		return EXIT_SUCCESS;
-	}
-
-	// read calibration config
-	cv::FileStorage fs;
-	fs.open(argv[1], cv::FileStorage::READ);
-	fs["Camera_device"] >> camera_device;
-	//cv::Size resolution_camera;
-	fs["Resolution_camera"] >> resolution_camera;
-	cv::Size resolution_projector;
-	fs["Resolution_projector"] >> resolution_projector;
-	cv::Mat camera_projector_transformation;
-	fs["Camera_projector_transformation"] >> camera_projector_transformation;
-	unsigned int frames_projector_camera_delay;
-	signed int int_frames_projector_camera_delay;
-	fs["Frames_projector_camera_delay"] >> int_frames_projector_camera_delay;
-	if (int_frames_projector_camera_delay < 0) {
-		frames_projector_camera_delay = 0;
-	} else {
-		frames_projector_camera_delay = (unsigned int) int_frames_projector_camera_delay;
-	}
-	double percentage_projector_background_light;
-	fs["Percentage_projector_background_light"] >> percentage_projector_background_light;
-	//float meter;
-	fs["Meter"] >> meter;
-	fs.release();
-
-	calibration = new Calibration(resolution_projector, camera_projector_transformation, frames_projector_camera_delay, percentage_projector_background_light);
-
-	// debug windows
-	cv::namedWindow("Camera", cv::WINDOW_NORMAL);
-	cv::moveWindow("Camera", 500, 0);
-
-	cv::namedWindow("Projection", cv::WINDOW_NORMAL);
-	cv::moveWindow("Projection", 1000, 0);
-
-	cv::namedWindow("Frame", cv::WINDOW_NORMAL);
-
-	// setup scene
-    sf::RenderWindow window(sf::VideoMode(resolution_projector.width, resolution_projector.height),"Projection");
-    window.clear(sf::Color::Black);
-    window.display();
-    
-    
-    LightTrailConfiguration config = LightTrailConfiguration::readFromFile(argv[2]);
-    scene = new LightTrailScene(config,
-                                       new LightSourceVectorRepository(),
-                                       new LightTrailVectorRepository(),
-                                       new GravityPointVectorRepository(),
-                                       new ColorHoleVectorRepository(),
-                                       new LightPersonMapRepository()
-                                       );
-
-	// setup clock
-    sf::Clock clock;
-
-	cv::waitKey(50);
-
-	// setup people extracting in seperate thread
-	std::thread thread_peopleextractor(main_peopleextractor);
-
-	// while no key pressed on other thread
-	while (running) {
 
 		// draw next scene frame based on clock difference
-		mutex_scene.lock();
 		float dt = clock.restart().asSeconds();
 		//float dt = 1.f/24.f;
 		scene->update(dt);
@@ -188,12 +166,10 @@ int main(int argc, char* argv[]) {
 		window.clear(sf::Color::Black);
 		scene->draw(window);
 
-		mutex_scene.unlock();
 		window.display();
 	}
 
-	// normally exit program by stopping people extractor thread
-	thread_peopleextractor.join();
+	video_capture.release();
 
 	return EXIT_SUCCESS;
 }
