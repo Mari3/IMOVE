a22eb36f299542cdc0a742dafcd6220713add4ac
Gerbert van Nieuwaal <gvannieuwaal@m-industries.com>
2016-05-31 11:55:44 +0200

    Stripped imove-scene of peopleextractor

--- imove-scene/src/ImoveSceneManager.hpp
+++ imove-scene/src/ImoveSceneManager.hpp
@@ -1,10 +1,9 @@
 #include "Calibration/Calibration.hpp"
 #include "Scene/LightTrail/LightTrailConfiguration.h"
-#include "ImageProcessing/PeopleExtractor.h"
 #include "Scene/Scene.h"
 
 // Setups people extractor and Scene, can let the Scene run using constant input of people extractor
-class ImoveManager {
+class ImoveSceneManager {
 	public:
 		/**
 		 * Setup people extractor and Scene.
@@ -12,13 +11,12 @@ class ImoveManager {
 		 * @param Calibration              The camera projector Calibration
 		 * @param configuration_lighttrail The light trail Scene configuration
 		 **/
-		ImoveManager(Calibration* calibration, LightTrailConfiguration& configuration_lighttrail);
+		ImoveSceneManager(Calibration* calibration, LightTrailConfiguration& configuration_lighttrail);
 
 		// Run the people extractor and Scene frame by frame
 		void run();
 
 	protected:
 		Calibration* calibration;
-		PeopleExtractor* people_extractor;
 		Scene* scene = NULL;
 };
--- imove-scene/src/main.cpp
+++ imove-scene/src/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-#include "ImoveManager.hpp"
+#include "ImoveSceneManager.hpp"
 #include "Calibration/Calibration.hpp"
 
 const unsigned int CONFIGURATION_CALIBRATION_ARGN = 1;
@@ -12,7 +12,7 @@ const unsigned int CONFIGURATION_LIGHTTRAIL_ARGN = 2;
 
 // setup and run Scene with continous people extraction as input based on configuration given in parameter otherwise show parameters
 int main(int argc, char* argv[]) {
-	// show parameters if not given 1 parameter
+	// show parameters if not given 2 parameter
 	if (argc != 3) {
 		std::cerr << "Usage: <path to configuration file> <path to configuration Scene>" << std::endl;
 		return EXIT_SUCCESS;
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
   
 	LightTrailConfiguration configuration_lighttrail = LightTrailConfiguration::readFromFile(argv[CONFIGURATION_LIGHTTRAIL_ARGN]);
 
-	ImoveManager manager(calibration, configuration_lighttrail);
+	ImoveSceneManager manager(calibration, configuration_lighttrail);
 	manager.run();
 	
 	return EXIT_SUCCESS;
