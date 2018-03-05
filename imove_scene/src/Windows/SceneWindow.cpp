fe53ed3068b387ec862ed3d70d8a819308c4f33d
Mari3 <mariekegeleers@Maries-MacBook-Pro.local>
2018-03-05 14:44:12 +0100

    Update to new versions of libraries

--- imove_scene/src/Windows/SceneWindow.cpp
+++ imove_scene/src/Windows/SceneWindow.cpp
@@ -21,12 +21,8 @@ SceneWindow::SceneWindow(cv::Size resolution_projector, const bool& fullscreen_p
 void SceneWindow::drawScene(Scene* scene) {
 	this->window_scene->clear(sf::Color::White);
 	scene->draw(*this->window_scene);
-	
-	this->window_scene->display();
-}
 
-const sf::Image SceneWindow::captureFrameScene() {
-	return this->window_scene->capture();
+	this->window_scene->display();
 }
 
 bool SceneWindow::shouldKeepOpen() {
